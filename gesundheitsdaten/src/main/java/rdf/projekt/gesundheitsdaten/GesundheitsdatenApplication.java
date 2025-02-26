d47ad7c5e52102f6a74bc9f442bc6659bec29aa8
mguezel <mehmet.guezel@rdf.nuernberg.de>
2025-02-26 12:06:22 +0100

    Added spring framework

---
+++ gesundheitsdaten/src/main/java/rdf/projekt/gesundheitsdaten/GesundheitsdatenApplication.java
@@ -0,0 +1,13 @@
+package rdf.projekt.gesundheitsdaten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesundheitsdatenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GesundheitsdatenApplication.class, args);
+	}
+
+}
---
+++ gesundheitsdaten/src/test/java/rdf/projekt/gesundheitsdaten/GesundheitsdatenApplicationTests.java
@@ -0,0 +1,13 @@
+package rdf.projekt.gesundheitsdaten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GesundheitsdatenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
